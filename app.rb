656f9604d3eee49e2187ab2c3d3f6854de2cc378
Emily Lillie <emilylillie3@gmail.com>
2019-08-25 14:22:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
   end 
   
   post '/checkout' do 
-    session[:item] = params[:item]
-    @session = session 
+    session[:item] = params[:item] #takes params from forms and adds to sessnion hash; key is item and value is item they picked 
+    @session = session #store as an instance variable so can access from views 
     erb :checkout 
     # where does that :checkout lead to?
   end 
